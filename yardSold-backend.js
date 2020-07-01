8ea64220aa7392e93b6a543d0ad3d8e5582333a4
Kern Elder <kelel1@yahoo.com>
2020-06-30 23:04:37 -0400

    add item schema

--- yardSold-backend.js
+++ yardSold-backend.js
@@ -1,10 +1,34 @@
-const { gql } = require("apollo-server")
-
 const { ApolloServer, gql } = require('apollo-server')
 
-const typedefs = gql`
-  type Vendor (
+const typeDefs = gql`
+  type Vendor {
     name: String!
-    phone: String
-  )
+    phone: String        
+    items: [Item]
+  }
+
+  type Item {
+    name: String!
+    price: String!
+    id: ID!
+  }
+  
+
+  type Query {
+    vendorCount: Int!
+  }
 `
+const resolvers = {
+  Query: {
+
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
